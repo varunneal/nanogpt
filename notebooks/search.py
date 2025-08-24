23d479d7d267bdb2d687388f393c8ef9f1c88f05
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-08-24 15:49:49 -0400

    hidden mayfly from hyperborea

--- notebooks/search.py
+++ notebooks/search.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os, pathlib, subprocess, time
+from tqdm.auto import tqdm
+
 
 NB = "1.8.3.2 nanogpt.ipynb"
 OUTDIR = "executed"
@@ -8,34 +10,41 @@ PARAMS = [
     {"SSEQ_LEN": 1024, "BATCH_SIZE": 8},
     {"SSEQ_LEN": 1024, "BATCH_SIZE": 4},
     {"SSEQ_LEN": 1024, "BATCH_SIZE": 2},
+    {"SSEQ_LEN": 1024, "BATCH_SIZE": 1},
 
-    {"SSEQ_LEN": 2048, "BATCH_SIZE": 8},
-    {"SSEQ_LEN": 2048, "BATCH_SIZE": 4},
-    {"SSEQ_LEN": 2048, "BATCH_SIZE": 2},
+    {"SSEQ_LEN": 2048, "BATCH_SIZE": 8, "LR_INIT": "1.414"},
+    {"SSEQ_LEN": 2048, "BATCH_SIZE": 4, "LR_INIT": "1.414"},
+    {"SSEQ_LEN": 2048, "BATCH_SIZE": 2, "LR_INIT": "1.414"},
 
     {"SSEQ_LEN": 512, "BATCH_SIZE": 32},
     {"SSEQ_LEN": 512, "BATCH_SIZE": 16},
     {"SSEQ_LEN": 512, "BATCH_SIZE": 8},
     {"SSEQ_LEN": 512, "BATCH_SIZE": 4},
     {"SSEQ_LEN": 512, "BATCH_SIZE": 2},
+    {"SSEQ_LEN": 512, "BATCH_SIZE": 1},
 
     {"SSEQ_LEN": 256, "BATCH_SIZE": 64},
     {"SSEQ_LEN": 256, "BATCH_SIZE": 32},
     {"SSEQ_LEN": 256, "BATCH_SIZE": 16},
     {"SSEQ_LEN": 256, "BATCH_SIZE": 8},
     {"SSEQ_LEN": 256, "BATCH_SIZE": 4},
+    {"SSEQ_LEN": 256, "BATCH_SIZE": 2},
+    {"SSEQ_LEN": 256, "BATCH_SIZE": 1},
 
     {"SSEQ_LEN": 128, "BATCH_SIZE": 128},
     {"SSEQ_LEN": 128, "BATCH_SIZE": 64},
     {"SSEQ_LEN": 128, "BATCH_SIZE": 32},
     {"SSEQ_LEN": 128, "BATCH_SIZE": 16},
     {"SSEQ_LEN": 128, "BATCH_SIZE": 8},
+    {"SSEQ_LEN": 128, "BATCH_SIZE": 4},
+    {"SSEQ_LEN": 128, "BATCH_SIZE": 2},
+
 ]
 
 os.makedirs(OUTDIR, exist_ok=True)
 base = pathlib.Path(NB).stem  # e.g., "1.8.3.2 nanogpt"
 
-for i, p in enumerate(PARAMS):
+for i, p in tqdm(enumerate(PARAMS)):
     env = os.environ.copy()
     env.update({k: str(v) for k, v in p.items()})  # set SSEQ_LEN, BATCH_SIZE, LR_INIT, RUN_NAME
     out = f"{base}_{i}.executed.ipynb"
